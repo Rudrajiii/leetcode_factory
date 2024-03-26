acc8f550c7b389d73007346e6a22ab1301513a23
rudrajiii <rudrasaha305@gmail.com>
2024-03-27 02:19:24 +0530

    binarySearch

--- DSA_ALGOS_JS/script.js
+++ DSA_ALGOS_JS/script.js
@@ -2,3 +2,42 @@
 //in JS
 
 
+//Basic Binary Search Algorithm
+
+function BinarySearch(arr , t){
+    let leftPointer = 0;
+    let rightPointer = arr.length - 1;
+    while(leftPointer <= rightPointer){
+        let midIdx = Math.floor((leftPointer + rightPointer) / 2);
+        if(arr[midIdx] === t){
+            return midIdx;
+        }
+        if(arr[midIdx] < t){
+            leftPointer = midIdx + 1;
+        }else{
+            rightPointer = midIdx - 1;
+        }
+    }
+    return -1;
+}
+console.log(BinarySearch([-5 , 2 , 4 , 6 , 10] , 6));
+console.log(BinarySearch([-5 , 2 , 4 , 6 , 10] , 10));
+console.log(BinarySearch([-5 , 2 , 4 , 6 , 10] , 11));
+
+function BinarySearchUsingRecursion(arr , t){
+    return logic(arr , t , 0 , arr.length - 1);
+}
+function logic(arr , t , lp , rp){
+    if(lp > rp) return -1;
+    let mi = Math.floor((lp + rp) / 2);
+    if(arr[mi] === t) return mi;
+    if(arr[mi] < t){
+        return logic(arr , t , mi + 1 , rp);
+    }else{
+        return logic(arr , t , lp , mi - 1);
+    }
+}
+
+console.log(BinarySearchUsingRecursion([-5 , 2 , 4 , 6 , 10] , 6));
+console.log(BinarySearchUsingRecursion([-5 , 2 , 4 , 6 , 10] , 10));
+console.log(BinarySearchUsingRecursion([-5 , 2 , 4 , 6 , 10] , 11));
