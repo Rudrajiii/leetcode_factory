18b8a2ba6904f45be00572ac52e0192d0cf7b9d7
_Rudra_Gojo_ <148994780+Rudrajiii@users.noreply.github.com>
2024-05-28 11:58:13 +0530

    Time: 59 ms (83.16%), Space: 50.9 MB (86.45%) - LeetHub

---
+++ 3408-count-the-number-of-special-characters-i/3408-count-the-number-of-special-characters-i.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var numberOfSpecialChars = function(word) {
+    let arr = []
+    for(let i of word){
+        if(word.includes(i.toUpperCase()) && word.includes(i.toLowerCase()) ){
+            if(i === i.toUpperCase()){
+                arr.push(i)
+            }
+        }
+    }
+    return new Set(arr).size;
+};
