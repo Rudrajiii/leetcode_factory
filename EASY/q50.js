b9f604c27244f357ea2059a5b2acd4f967556af2
rudrajiii <rudrasaha305@gmail.com>
2024-05-29 22:35:31 +0530

    v3 push

---
+++ EASY/q50.js
@@ -0,0 +1,19 @@
+
+// 2824. Count Pairs Whose Sum is Less than Target
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countPairs = function(nums, target) {
+    let count = 0;
+    nums.forEach((num, i) => {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (num + nums[j] < target) {
+                count++;
+            }
+        }
+    });
+    return count;
+};
