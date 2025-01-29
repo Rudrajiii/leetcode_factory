df5e0223cf7d068ffab0716298f9245062589a71
rudrajiii <rudrasaha305@gmail.com>
2025-01-30 00:14:00 +0530

    replacement fixed--

---
+++ C/C_DSA_QS_EASY/PROB_3024_Type_Of_Triangle.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+char* triangleType(int* nums, int numsSize) {
+    int a = nums[0] , b = nums[1] , c = nums[2];
+    if(a + b <= c || a + c <= b || b + c <= a){
+        return "none";
+    }
+    if(a == b && a == c){
+        return "equilateral";
+    }
+    if(a == b || a == c || b == c){
+        return "isosceles";
+    }
+    return "scalene";
+}
