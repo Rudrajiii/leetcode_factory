d0a1b3bc27c7c730c53c2d8d4a4dc6a8d92e0c99
rudrajiii <rudrasaha305@gmail.com>
2025-02-01 23:42:19 +0530

    1 qs modified

--- PYTHON/EASY_PY_LEETCODE/PROB_278_First_Bad_Version.py
+++ PYTHON/EASY_PY_LEETCODE/PROB_278_First_Bad_Version.py
@@ -1,5 +1,6 @@
 # The isBadVersion API is already defined for you.
 # def isBadVersion(version: int) -> bool:
+
 def isBadVersion(mid):...
 class Solution:
     def firstBadVersion(self, n: int) -> int:
